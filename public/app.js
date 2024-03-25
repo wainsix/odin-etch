9b34e8aad0a00c572338d7a5dba8b60eac511330
Wainsix <wainsix@gmail.com>
2024-03-25 10:45:55 +0200

    s

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 function addElement() {
   const conDiv = document.getElementById("container");
 
-  for (let i = 1; i < 17; i++) {
+  for (let i = 1; i <= 16; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("div" + i);
     conDiv.appendChild(newDiv);
