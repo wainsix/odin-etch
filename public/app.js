44c7b33ac936bf72921758f58b43d2b730956014
Wainsix <wainsix@gmail.com>
2024-03-25 11:02:34 +0200

    clear-button

--- public/app.js
+++ public/app.js
@@ -1,9 +1,16 @@
 function addElement() {
   const conDiv = document.getElementById("container");
+  conDiv.innerHTML = "";
 
   for (let i = 1; i <= 16; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("div" + i);
+    newDiv.addEventListener("mouseenter", function () {
+      newDiv.classList.add("hovered"); // Lisää luokka "hovered" kun hiiri on elementin päällä
+    });
+
+    document.getElementById("btnClr").addEventListener("click", addElement);
+
     conDiv.appendChild(newDiv);
   }
 }
